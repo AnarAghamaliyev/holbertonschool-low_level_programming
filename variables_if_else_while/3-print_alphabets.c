b82a7e6a1edb374219aa6f4bd8c9c86ef26e0d47
AnarAghamaliyev <anaraghamaliyev7@gmail.com>
2025-12-28 03:25:59 +0400

    Update and rename 3-print_alpabets.c to 3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,25 +1,46 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabetic in lower and upper case
+ * main - Prints the alphabet in lowercase, then uppercase,
+ * followed by a new line
  *
- * Return: Always (Success)
+ * Return: Always 0
  */
 int main(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-	{
 		putchar(c);
-	}
 
 	for (c = 'A'; c <= 'Z'; c++)
-	{
 		putchar(c);
-	}
 
 	putchar('\n');
 
 	return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
